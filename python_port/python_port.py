c36f4272c8ed75cf259525dba8f7dd87f777f454
danieldaga <daniel@arhrestauracion.com>
2023-12-19 20:29:09 +0100

    components

--- python_port/python_port.py
+++ python_port/python_port.py
@@ -4,7 +4,11 @@ class State(rx.State):
     pass
 
 def index() -> rx.Component:
-    return rx.text("Hola Reflex!", color="blue", fontSize="2em")
+    return rx.button_group(
+        rx.button("Button 1", color="red"),
+        rx.button("Button 2", color="blue"),
+        rx.button("Button 3", color="green"),
+    )
 
 app = rx.App()
 app.add_page(index)
