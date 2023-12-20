d373fe186ef82d73be4da6bab0e0ee9de13f20a0
danieldaga <daniel@arhrestauracion.com>
2023-12-20 18:57:25 +0100

    colors header

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # python-port
+
+# colorsss
+
+shadow
+HEX: #314299
+RGB: rgb(49,66,153)
+
+claro
+HEX: #00BBF9
+RGB: rgb(0,187,249)
+
+blue sky
+
+HEX: #A6C8E3
+RGB: rgb(166,200,227)
+
+
+alter
+HEX: #F15BB5
+RGB: rgb(241,91,181)
--- python_port/components/navbar.py
+++ python_port/components/navbar.py
@@ -4,10 +4,12 @@ def navbar() -> rx.Component:
     return rx.hstack(
         rx.text(
             "Daniel García Developer",
-            height="40px"
+            height="40px",
             ),
+            width="full",
             position="sticky",
-            bg="blue",
+            bg="#314299",
+            color="#00BBF9",
             padding_x="16px",
             z_index="5",
         )
